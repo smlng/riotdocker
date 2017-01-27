8d2b28d467c568ee3a37f25bd93e422d195fba9e
Kaspar Schleiser <kaspar@schleiser.de>
2017-01-27 12:14:57 +0100

    ...

--- start_murdock_slave.sh
+++ start_murdock_slave.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 exec docker run --rm -t -i -u $(id -u) \
-	--tmpfs /tmp:rw,exec,size=8g \
+    --tmpfs /tmp:rw,exec,size=8g \
     -v ${HOME}/.ssh:/data/riotbuild/.ssh \
-	-v ${HOME}/.gitcache:/data/riotbuild/.gitcache \
-	-v ${HOME}/.ccache:/data/riotbuild/.ccache \
+    -v ${HOME}/.gitcache:/data/riotbuild/.gitcache \
+    -v ${HOME}/.ccache:/data/riotbuild/.ccache \
     -e CCACHE="ccache" \
-	-e "DWQ_SSH=${DWQ_SSH}" \
-	riotdocker-dwq $*
+    -e "DWQ_SSH=${DWQ_SSH}" \
+    --security-opt seccomp=unconfined \ # needed for 32bit getcontext() in native to work properly
+    riotdocker-dwq $*
