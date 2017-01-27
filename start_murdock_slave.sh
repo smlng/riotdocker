2e4412f98204e8cc237a568003f25b8043b67e28
Kaspar Schleiser <kaspar@schleiser.de>
2017-01-27 17:10:21 +0100

    ...

--- start_murdock_slave.sh
+++ start_murdock_slave.sh
@@ -4,12 +4,12 @@ MURDOCK_HOSTNAME=${MURDOCK_HOSTNAME:-$(hostname)}
 MURDOCK_USER=${MURDOCK_USER:-${USER}}
 MURDOCK_HOME=$(eval echo ~${MURDOCK_USER})
 MURDOCK_QUEUES=${MURDOCK_QUEUES:-${MURDOCK_HOSTNAME} default}
-#MURDOCK_EXTRA_ARGS="--tmpfs /tmp:size=4g,exec,nosuid"
+#MURDOCK_DOCKER_ARGS="--tmpfs /tmp:size=4g,exec,nosuid"
 
-exec docker run --rm -t -i -u $(id -u ${MURDOCK_USER}) \
+exec docker run --rm -u $(id -u ${MURDOCK_USER}) \
     -v ${MURDOCK_HOME}:/data/riotbuild \
-    ${MURDOCK_EXTRA_ARGS} \
+    ${MURDOCK_DOCKER_ARGS} \
     -e CCACHE="ccache" \
     -e DWQ_SSH \
     --security-opt seccomp=unconfined \
-    riotdocker-dwq murdock_slave --name $MURDOCK_HOSTNAME --queues ${MURDOCK_QUEUES}
+    riotdocker-dwq murdock_slave --name $MURDOCK_HOSTNAME --queues ${MURDOCK_QUEUES} "$@"
