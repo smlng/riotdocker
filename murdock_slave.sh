490dd7b062e0a6a1cd12cdf172540835f72e7326
Kaspar Schleiser <kaspar@schleiser.de>
2017-02-22 16:38:45 +0100

    dwq: some murdock_slave.sh fixes

--- murdock_slave.sh
+++ murdock_slave.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-autossh -f -N -L7711:localhost:7711 \
+autossh -M 0 -f -C -N -L7711:localhost:7711 \
     -o ServerAliveInterval=60 -o ServerAliveCountMax=2 \
     ${DWQ_SSH:-murdock}
 
-git-cache init
+git-cache init || {
+    echo "Error initializing git-cache. Permission problem?"
+    exit 1
+}
 
 exec dwqw $*
