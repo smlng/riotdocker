fd9436255e41d5700225fd8c6187249cfe46bba8
Kaspar Schleiser <kaspar@schleiser.de>
2017-03-07 14:07:34 +0100

    bump default tmpfs to 2g

--- start_murdock_slave.sh
+++ start_murdock_slave.sh
@@ -4,7 +4,7 @@ MURDOCK_HOSTNAME=${MURDOCK_HOSTNAME:-$(hostname)}
 MURDOCK_USER=${MURDOCK_USER:-murdock}
 MURDOCK_HOME=$(eval echo ~${MURDOCK_USER})
 MURDOCK_QUEUES=${MURDOCK_QUEUES:-${MURDOCK_HOSTNAME} default}
-MURDOCK_DOCKER_ARGS=${MURDOCK_DOCKER_ARGS:-"--tmpfs /tmp:size=1g,exec,nosuid"}
+MURDOCK_DOCKER_ARGS=${MURDOCK_DOCKER_ARGS:-"--tmpfs /tmp:size=2g,exec,nosuid"}
 
 exec docker run --rm -u $(id -u ${MURDOCK_USER}) \
     -v ${MURDOCK_HOME}:/data/riotbuild \
